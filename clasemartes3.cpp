b9a902ab15a5fbdcef18b76fefc8f1325d0fc3fd
SebastianSebz <37335472+SebastianSebz@users.noreply.github.com>
2018-04-03 13:09:45 +0000

    Add files via upload

---
+++ clasemartes3.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <cstdlib>
+#include <ctime>
+
+using std::cout;
+using std::endl;
+
+
+
+int main(){
+
+int valor; int i=0; int suma;
+int sumanum(valor);
+
+int sumanum(valor){
+	for(i;i<(valor+1);i++){
+	suma = suma+i;}
+}
+cout << "Suma valor 50: " << sumanum(50) << endl;
+cout << "Suma valor 100: " << sumas(100) << endl;
+}
